12b069e1c3d98596d911ef65f001c08112e4d3be
Ben Carson <quod27@gmail.com>
2016-10-13 17:10:56 -0500

    Stub CSVParserTest

---
+++ src/test/java/edu/jsu/mcis/CSVParserTest.java
@@ -0,0 +1,21 @@
+package edu.jsu.mcis;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import au.com.bytecode.opencsv.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CSVParserTest {
+    public void testParserReadsStudentFile() {
+        assertTrue(false);
+    }
+
+    public void testParserReadsCourseFile() {
+        assertTrue(false);
+    }
+
+    public void testParserReadsSingleLine() {
+        assertTrue(false);
+    }
+}
