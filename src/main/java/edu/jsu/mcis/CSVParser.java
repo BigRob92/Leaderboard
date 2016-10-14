35094f13306790968070ef6a63a0944ac144391c
Ben Carson <quod27@gmail.com>
2016-10-13 19:02:18 -0500

    Progress Gamegogy class

--- src/main/java/edu/jsu/mcis/CSVParser.java
+++ src/main/java/edu/jsu/mcis/CSVParser.java
@@ -4,6 +4,9 @@ import java.lang.*;
 import au.com.bytecode.opencsv.*;
 
 public class CSVParser {
+    //public CSVParser(String arg){
+    //}
+
     public void studentParser() throws Exception {
         CSVReader reader = new CSVReader(new FileReader("students.csv"));
         String [] nextLine;
@@ -23,10 +26,12 @@ public class CSVParser {
     public void studentLine(String studentId) throws Exception {
         CSVReader reader = new CSVReader(new FileReader("students.csv"));
         String [] nextLine;
+        String foo = studentId;
         while ((nextLine = reader.readNext()) != null) {
-            while (!studentId.equals(nextLine[0])) {
-
-            } System.out.println(nextLine[0] + nextLine[1] + nextLine[2] + nextLine[3]);
+            if (studentId.equals(nextLine[0])) {
+            System.out.println(nextLine[0] + nextLine[1] + nextLine[2] + nextLine[3]);
+            }
+            else {System.out.println("Student not found.");}
         }
     }
 
@@ -34,9 +39,11 @@ public class CSVParser {
         CSVReader reader = new CSVReader(new FileReader("courses.csv"));
         String [] nextLine;
         while ((nextLine = reader.readNext()) != null) {
-            while (!courseId.equals(nextLine[0])) {
+            if (courseId.equals(nextLine[0])) {
+                System.out.println(nextLine[0] + nextLine[1] + nextLine[2] + nextLine[3]);
+            }
 
             } System.out.println(nextLine[0] + nextLine[1] + nextLine[2] + nextLine[3]);
         }
-    }
+
 }
--- src/main/java/edu/jsu/mcis/Gamegogy.java
+++ src/main/java/edu/jsu/mcis/Gamegogy.java
@@ -5,14 +5,34 @@ import java.util.*;
 //import au.com.bytecode.opencsv.*;
 
 public class Gamegogy {
-private DataSource d = new DataSource();
-    
+    public Gamegogy() {
+        DataSource d = new DataSource();
+        Student s = new Student("","","","");
+        Course c = new Course("","","","");
+    }
+
     public static void main(String[] args) throws Exception {
-        Gamegogy g = new Gamegogy();
-        
-        for (String s: args) {
-            if (s.equals("studentids")) {
+        CSVParser parser = new CSVParser();
+        for (String arg : args) {
+            if (arg.equals("studentids")) {
+                parser.studentParser();
             }
+            else if (arg.equals("courseids")) {
+                parser.courseParser();
+            }/*
+            else if (arg.equals("student")) {
+                foo = s.setStudentId(arg[1]);
+                parser.studentLine(foo);
+
+            }
+            else if (arg.equals("course")) {
+                bar = c.setCourseId(arg[1]);
+                parser.courseLine(bar);
+            }
+            else {System.out.println("Error");}
+            */
+        //for (String s: args) {
+        //    if (s.equals("studentids")) 
         }
     }
 }
